2a15db8b9391ab639e767ea286cca6f8c52538d4
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-10-16 12:50:18 +0900

    Generics wrapper of sync.Map

---
+++ generics/sync_map.go
@@ -0,0 +1,65 @@
+package generics
+
+import "sync"
+
+type SyncMap[K comparable, V any] struct {
+	// type conversion protection, see atomic.Pointer
+	_ [0]*K
+	_ [0]*V
+
+	m sync.Map
+}
+
+func (m *SyncMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
+	return m.m.CompareAndDelete(key, old)
+}
+
+func (m *SyncMap[K, V]) CompareAndSwap(key K, old V, new V) bool {
+	return m.m.CompareAndSwap(key, old, new)
+}
+
+func (m *SyncMap[K, V]) Delete(key K) {
+	m.m.Delete(key)
+}
+
+func (m *SyncMap[K, V]) Load(key K) (value V, ok bool) {
+	if v, o := m.m.Load(key); o {
+		return v.(V), true
+	} else {
+		return
+	}
+}
+
+func (m *SyncMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+	if v, l := m.m.LoadAndDelete(key); l {
+		return v.(V), true
+	} else {
+		return
+	}
+}
+
+func (m *SyncMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
+	if a, l := m.m.LoadOrStore(key, value); l {
+		return a.(V), true
+	} else {
+		return value, false
+	}
+}
+
+func (m *SyncMap[K, V]) Range(f func(key K, value V) bool) {
+	m.m.Range(func(key, value any) bool {
+		return f(key.(K), value.(V))
+	})
+}
+
+func (m *SyncMap[K, V]) Store(key K, value V) {
+	m.m.Store(key, value)
+}
+
+func (m *SyncMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+	if p, l := m.m.Swap(key, value); l {
+		return p.(V), true
+	} else {
+		return
+	}
+}
---
+++ generics/sync_map_test.go
@@ -0,0 +1,101 @@
+package generics_test
+
+import (
+	"testing"
+
+	"github.com/free5gc/util/generics"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSyncMap(t *testing.T) {
+	var m generics.SyncMap[int, string]
+
+	// Load and Store
+	m.Store(1, "test")
+	v, e := m.Load(1)
+	assert.True(t, e)
+	assert.Equal(t, "test", v)
+	_, e = m.Load(2)
+	assert.False(t, e)
+
+	// Delete
+	m.Store(2, "test2")
+	m.Delete(1)
+	_, e = m.Load(1)
+	assert.False(t, e)
+	m.Delete(3)
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+
+	// LoadOrStore
+	v, e = m.LoadOrStore(1, "test")
+	assert.False(t, e)
+	assert.Equal(t, "test", v)
+	v, e = m.Load(1)
+	assert.True(t, e)
+	assert.Equal(t, "test", v)
+	v, e = m.LoadOrStore(2, "testX")
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+
+	// LoadAndDelete
+	v, e = m.LoadAndDelete(1)
+	assert.True(t, e)
+	assert.Equal(t, "test", v)
+	_, e = m.Load(1)
+	assert.False(t, e)
+	_, e = m.LoadAndDelete(1)
+	assert.False(t, e)
+
+	// Swap
+	_, e = m.Swap(1, "test")
+	assert.False(t, e)
+	v, e = m.Load(1)
+	assert.True(t, e)
+	assert.Equal(t, "test", v)
+	v, e = m.Swap(2, "testX")
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "testX", v)
+
+	// CompareAndDelete
+	m.Delete(1)
+	m.Store(2, "test2")
+	assert.False(t, m.CompareAndDelete(2, "testX"))
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+	assert.True(t, m.CompareAndDelete(2, "test2"))
+	_, e = m.Load(2)
+	assert.False(t, e)
+
+	// CompareAndSwap
+	m.Store(2, "test2")
+	assert.False(t, m.CompareAndSwap(1, "test", "testX"))
+	_, e = m.Load(1)
+	assert.False(t, e)
+	assert.False(t, m.CompareAndSwap(2, "testX", "testY"))
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "test2", v)
+	assert.True(t, m.CompareAndSwap(2, "test2", "testY"))
+	v, e = m.Load(2)
+	assert.True(t, e)
+	assert.Equal(t, "testY", v)
+
+	m.Range(func(key int, value string) bool {
+		switch key {
+		case 2:
+			assert.Equal(t, "testY", value)
+		default:
+			assert.Fail(t, "Invalid key %d", key)
+		}
+		return true
+	})
+}
