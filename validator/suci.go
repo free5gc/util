c8f3e5f9af8c0e7c02d4df2a7a920e00a8840ec1
roundspring2003 <hwilly40159@gmail.com>
2025-12-31 00:39:02 +0800

    fix: golint

--- validator/suci.go
+++ validator/suci.go
@@ -8,7 +8,8 @@ import (
 // TS 23.003 28.7.3
 // SUCI format validation
 // suci-<MCC>-<MNC>-<Routing Indicator>-<Protection Scheme>-<Home Network Public Key Id>-<Scheme Output>
-var suciRegex = regexp.MustCompile(`^suci-[0-9]{3}-[0-9]{2,3}-[a-fA-F0-9]{1,4}-[a-fA-F0-9]{1,2}-[a-fA-F0-9]{1,2}-[a-fA-F0-9]+$`)
+var suciRegex = regexp.MustCompile(`^suci-[0-9]{3}-[0-9]{2,3}-[a-fA-F0-9]{1,4}-` +
+	`[a-fA-F0-9]{1,2}-[a-fA-F0-9]{1,2}-[a-fA-F0-9]+$`)
 
 // TS 23.003 28.7.2
 // NAI format validation (SUCI can also be in NAI format)
