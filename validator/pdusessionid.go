2d4a35e9683655657ce3e5e0cdbaca10a4556096
chchen7 <chchen.cs14@nycu.edu.tw>
2026-01-10 20:47:44 +0800

    feat: add pdu session id validator

---
+++ validator/pdusessionid.go
@@ -0,0 +1,9 @@
+package validator
+
+import "strconv"
+
+// TS 29.571 5.4.2 valid PDU Session ID is an integer in the range 0 to 255
+func IsValidPduSessionID(pduSessionID string) bool {
+	pduSessionIDInt, err := strconv.Atoi(pduSessionID)
+	return err == nil && pduSessionIDInt >= 0 && pduSessionIDInt < 256
+}
